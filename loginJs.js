4fe0a9e4159fdcd88692e68903728461589c431e
nahuelbonifacino <nahuelbonifacino98@gmail.com>
2024-06-05 01:39:13 -0300

    Creacion de archivos

---
+++ loginJs.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("button");
+
+    button.addEventListener("click", () => {
+        const user = document.getElementById("user").value;
+        const password = document.getElementById("password").value;
+
+        fetch('apiPrueba.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const usuarioEncontrado = data.usuarios.find(usuario => usuario.user === user);
+                if (usuarioEncontrado && usuarioEncontrado.password === password) {
+                    alert("Exito");
+                    window.location.href = "CoberFox.html"
+                } else {
+                    alert("Usuario o contraseña incorrectos");
+                }
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    });
+});
